b4738622f1fecf7c98d002371171eab4a083cf2a
Jitendra <jitendrakumarbindra760@gmail.com>
2026-01-02 12:06:00 -0800

    fixed cors

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://cors-prettier-crud-app-backend.vercel.app",
-        changeOrigin: true,
-        secure: true, // since your backend is HTTPS
-      },
-    },
-  },
 });
