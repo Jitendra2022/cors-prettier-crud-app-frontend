642f2f71311ec94d52f96184ee34080c58a67966
Jitendra <jitendrakumarbindra760@gmail.com>
2026-01-02 22:04:51 -0800

    spelling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white text-center p-4">
-      &copy; 2026 Vamica Private Limited Co.. All rights reserved.
+      &copy; 2026 Vamika Private Limited Co.. All rights reserved.
     </footer>
   );
 };
--- src/components/StickyHeader .jsx
+++ src/components/StickyHeader .jsx
@@ -4,7 +4,7 @@ const StickyHeader = () => {
   return (
     <>
       <h1 className="bg-[#70026a] text-white shadow h-24 flex items-center justify-center font-bold">
-        Vamica Private Limited Co.
+        Vamika Private Limited Co.
       </h1>
     </>
   );
